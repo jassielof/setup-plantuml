53e3c38c0d797db463304922832b23a5c56b0f1c
Jassiel Ovando <jassielovando@protonmail.com>
2026-02-02 22:17:15 -0400

    scripts, tests, and such

---
+++ scripts/download.bash
@@ -0,0 +1,16 @@
+set -e
+
+VERSION="${VERSION:-latest}"
+INSTALL_DIR="${INSTALL_DIR:-$GITHUB_WORKSPACE/.plantuml}"
+
+mkdir -p "$INSTALL_DIR"
+
+if [ "$VERSION" = "latest" ]; then
+  URL="https://github.com/plantuml/plantuml/releases/download/latest/plantuml.jar"
+else
+  URL="https://github.com/plantuml/plantuml/releases/download/v${VERSION}/plantuml-${VERSION}.jar"
+fi
+
+echo "Downloading PlantUML from: $URL"
+curl -L --fail -o "$INSTALL_DIR/plantuml.jar" "$URL"
+echo "Downloaded to: $INSTALL_DIR/plantuml.jar"
---
+++ scripts/plantuml.cmd
@@ -0,0 +1,1 @@
+@java -jar "%GITHUB_WORKSPACE%\.plantuml\plantuml.jar" %*
